ce31564e5952df3e933c90e5e11b6539f3901b32
Joshua Rasband <joshuarasband@Joshuas-Laptop.local>
2025-05-06 17:21:21 -0500

    make exported html more minimal

--- build-blog.el
+++ build-blog.el
@@ -1,11 +1,19 @@
 (require 'ox-publish)
+(require 'htmlize)
+
+(setq org-html-validation-link nil)
+(setq org-html-head-include-scripts nil)
+(setq org-html-head-include-default-style nil)
+;; (setq org-html-preamble-format '(("en" "")))
+(setq org-html-postamble-format '(("en" "")))
+(setq org-html-indent t)
 
 (setq org-publish-project-alist
       '(("content-org"
          :base-directory "~/Projects/jrasband-blog/content-org/"
          :base-extension "org"
          :publishing-directory "~/Projects/jrasband-blog/public/"
-         :publishing-function org-html-publish-to-html         
+         :publishing-function org-html-publish-to-html
          :recursive t
          :section-numbers nil
          :with-toc nil
@@ -14,7 +22,8 @@
          :auto-sitemap t
          :sitemap-filename "index.org"
          :sitemap-title "Joshua Rasband's blog"
-         :html-head "<link rel=\"stylesheet\" href=\"css/stylesheet.css\" type=\"text/css\"/>"
+         :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+         ;; :html-head "<link rel=\"stylesheet\" href=\"css/stylesheet.css\" type=\"text/css\"/>"
          ;;:sitemap-function jem/sitemap-format ;; TODO fix
          :sitemap-sort-files alphabetically)
         
@@ -27,6 +36,6 @@
 
         ("blog" :components ("content-org" "blog-static"))))
 
-;; (org-publish "blog")
+(org-publish "blog")
 
 ;; (denote-directory-files "_blogpost")
